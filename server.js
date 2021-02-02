d9a32e6e1c728a3915b44235e8a8c4fcbcf8c5a0
pepsi-c <69380680+pepsi-c@users.noreply.github.com>
2021-02-02 15:44:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 //const fs = require('fs');
-
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
 
 const verifyj = JSON.parse(fs.readFileSync("./verify.json", "utf8"))
@@ -132,5 +133,6 @@ let rf = message.guild.roles.find('name', `${verifyj[message.guild.id].rolev}`)
         if (err) console.error(err)
       })
     })
-}})                           
+}})   
 
+bot.login(process.env.BOT_TOKEN);
